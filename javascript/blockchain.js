8c11a6e28b3e1a120a99d886e72ef973d0f1d6d3
elidielpoltronieri/contratos <56139039+elidielpoltronieri@users.noreply.github.com>
2019-12-08 16:00:41 -0300

    Update blockchain.js

--- javascript/blockchain.js
+++ javascript/blockchain.js
@@ -1,72 +1,76 @@
-<!DOCTYPE html>
-<html lang="pt">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Leilão</title>
- </head>
-<body>
-    <h1>Leilão</h1>
-<div>O número do contrato é: "0xbc6573af5627d0d0ff317379b93131176ffe2da0"</div>
-<br/>
-<br/>
-   <h2>Status do Leilão</h2>
-    <div>O bem em oferta é: <span id="campoBem">&nbsp;</span>
-    </div>
-    <div>O lance mínimo é: <span id="campoLance">&nbsp;</span>
-    </div>
-    <br/>
-    <div>O valor do último Lance é: <span id="maiorLance">&nbsp;</span>
-    </div>
-    <br/>
-    <hr>
-        </div>
-        <div>
-            &nbsp;
-    </form>
-    <h1>Lances</h1>
-    <form name="frmLance" id="frmLance" action="#" method="get" onsubmit="executeLance(); return false;">
-        <div>
-            <label for="Lance">Oferte um novo Lance: </label>
-            <input id="Lance" type= "number">
-        </div>
-        <div>
-            &nbsp;
-        </div>
-        <div>
-            <button type="submit">Registrar Lance</button>
-        </div>
-        <div><br><hr></div>
-        <div id="campoLance">
-            &nbsp;
-        </div>
-    </form>
+var enderecoContrato = "0xbc6573af5627d0d0ff317379b93131176ffe2da0";
+var provedor = new ethers.providers.Web3Provider(web3.currentProvider);
+ethereum.enable();
+var signatario = provedor.getSigner();
+var contrato = new ethers.Contract(enderecoContrato, abiContrato, signatario);
 
-    <p>&nbsp;</p>
-    
-    <script charset="utf-8"  src="https://cdn.ethers.io/scripts/ethers-v4.min.js" type="text/javascript"></script>
-    <script src="javascript/abi.js"></script>    
-    <script src="javascript/blockchain.js"></script>    
-    <script>
-<h1>Encerramento do leilao</h1>
-    <form action="#" method="get" id="frmEncerrar" name="frmEncerrar" onsubmit="encerrarLeilão(); return false;">
-        <div>
-        <br>
-        </div>
-        <div>
-            &nbsp;
-        </div>
-        <div>
-            <button type="submit">Encerrar Leilão</button>
-        </div>
-        <div><br><hr></div>
-        <div id="encerrarLeilão">
-            &nbsp;
-        </div>
-    </form>
-        
-    </script>
+function executeLance() {
+    var Lance = document.frmLance.Lance.value;
+    if (Lance<maiorLance>) 
+    {
+        alert ("Valor inferior ao último Lance")
+        return false;
+    }
+    var Ofertante = document.frmLance.Ofertente.value;
+    var campoBem = document.BemById("campoBem"); 
+    var campoLance = document.LanceById("campoLance");
+    campoLance.innerHTML = "Sending transaction...";
+    var additionalSettings = {
+        value: ethers.utils.parseUnits(lance, 'wei')
+    }; 
+    contrato.Lance(ofertante, additionalSettings)
+    .then( (resultado) => 
+        {
+        console.log("executeLance - Transaction ", tx);
+        campoLance.innerHTML = "Transação enviada. Aguardando o resultado ...";
+        tx.wait ()
+        .then( (resultFromContract) => {
+            console.log("executeLance - O resultado é", resultFromContract);
+            executeLance();
+            campoLance.innerHTML = "Transação executada.";
+        })
 
-</body>
-</html>
+    .catch( (err) => 
+    {
+        console.error("executeLance - após lance ser minerada");
+        console.error(err);
+        campoLance.innerHTML = "Algo saiu errado: " + err.message;
+    })
+    .catch( (err) => {
+        console.error("executeLance - lance enviada");
+        console.error(err);
+        campoLance.innerHTML = "Algo deu errado" + err.message;
+    })
+}
+
+
+        contrato.lanceMinimo()
+            .then( (Lance) => 
+            {
+                campoLance.innerHTML = Lance;
+            })
+            .catch( (err) => 
+            {
+                console.error(err);
+                campoLance.innerHTML = err;
+            });       
+  
+         contrato.maiorLance()
+            .then( (Lance) => 
+            {
+                campomaiorLance.innerHTML = Lance;
+            })
+            .catch( (err) => 
+            {
+                console.error(err);
+                campomaiorLance.innerHTML = err;
+            });       
+               
+            var campoBem = 
+            ">&nbsp;</span>
+                </div>
+                <div>O lance mínimo é: <span id="campoLance">&nbsp;</span>
+                </div>
+                <br/>
+                <div>O valor do último Lance é: <span id="maiorLance">&nbsp;</span>
+            
